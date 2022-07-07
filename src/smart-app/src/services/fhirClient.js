38f496b0b0c2090189b2b0c7a5e59431da95dc83
Adedayo Adebiyi <dayo@Adedayos-MacBook-Pro.local>
2022-07-07 07:48:55 -0500

    added fhir service

---
+++ src/smart-app/src/services/fhirClient.js
@@ -0,0 +1,16 @@
+import FHIR from 'fhirclient';
+
+let client;
+
+const connect = async()=>{
+    if(client){
+        return client;
+    }
+
+    return FHIR.oauth2.ready().then((smart)=>{
+        client = smart;
+        return client;
+    });
+}
+
+export default connect;
